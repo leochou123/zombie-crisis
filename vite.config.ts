0c14c4c19c938d66a138b887aaf114f84fcb8242
leochou123 <137378348+leochou123@users.noreply.github.com>
2026-06-29 16:52:00 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/messi-world-cup/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
